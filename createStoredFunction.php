c4da2ea2269f54c79d554c121f1b8b86f0b36b40
vjaro <vincent.jaro@devtac.asia>
2020-04-09 01:52:17 +0800

    Initial commit

---
+++ createStoredFunction.php
@@ -0,0 +1,55 @@
+<?php
+
+if (!defined('sugarEntry')) {
+    define('sugarEntry', true);
+}
+require_once('include/entryPoint.php');
+
+function getPDOConnection()
+{
+    global $sugar_config, $db;
+
+    $config = new Doctrine\DBAL\Configuration();
+    $params = array(
+        'dbname' => $sugar_config['dbconfig']['db_name'],
+        'user' => $sugar_config['dbconfig']['db_user_name'],
+        'password' => $sugar_config['dbconfig']['db_password'],
+        'host' => $sugar_config['dbconfig']['db_host_name'],
+        'port' => $sugar_config['dbconfig']['db_port'],
+        'driver' => 'pdo_mysql',
+        'driverOption' => array(
+            PDO::ATTR_EMULATE_PREPARES => true
+        )
+    );
+    $conn = $db->getConnection();
+    try {
+        $pdoCon = Doctrine\DBAL\DriverManager::getConnection($params, $config);
+        if ($pdoCon) {
+            $driver = $pdoCon->getDriver();
+            $conn = $pdoCon;
+        }
+    } catch (Exception $e) {
+        $GLOBALS["log"]->fatal($e->getMessage());
+    }
+
+    return $conn;
+}
+
+function createStoredFunction()
+{
+    $conn = getPDOConnection();
+    if($conn){
+        $query = "getAge.sql";
+        $contents = file_get_contents($query);
+        $stmt = $conn->prepare($contents);
+        $res = $stmt->execute();
+        if (!$res){
+            return "failed to install stored function \n";
+        }else{
+            return "successfully installed stored function \n";
+        }
+    }
+
+}
+
+print_r(createStoredFunction());
